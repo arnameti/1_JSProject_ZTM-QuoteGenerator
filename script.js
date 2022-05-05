81e09300e45f16f8c4541e3db75913ffd45383bf
Arnor Ameti <arnameti@gmail.com>
2022-05-05 18:13:36 +0200

    Manipulating DOM

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 "use strict";
 
+const quoteContainer = document.querySelector(".quote-container");
+const quoteText = document.querySelector(".quote-container__quote");
+const author = document.querySelector(".quote-container__author");
+
+const twitterBtn = document.querySelector(".buttons-container__twitter-btn");
+const newQuoteBtn = document.querySelector(".buttons-container__new-quote-btn");
+
 let apiQuotes = [];
 
 // Show new quote
-const newQuote = function () {
-  const getRandomInt = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+const newQuote = () => {
+  const getRandomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
 
   const quote = apiQuotes[getRandomInt(0, apiQuotes.length - 1)];
-  return quote;
+
+  author.textContent = quote.author ? quote.author : "Author not known";
+
+  if (quote.text.length > 120) quoteText.classList.toggle("long-quote");
+  quoteText.textContent = quote.text;
 };
-
-
 
 // Get Quotes from API
 const getQuotes = async function () {
@@ -24,7 +32,15 @@ const getQuotes = async function () {
   } catch (error) {}
 };
 
+// Twee Quote
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
+  window.open(twitterUrl, "_blank");
+};
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
 // On Load
 getQuotes();
-
-
